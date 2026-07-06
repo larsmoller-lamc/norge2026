fe2ca4379b67e3912fcc2a44bf6153db9c6e32d6
larsmoller-lamc <larsmollerchristensen@gmail.com>
2026-07-06 10:53:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,22 @@
    ===================================================================== */
 
 window.APP_CONFIG = {
-  apiKey: "AIzaSyCgbP4u08tBeNhjIW7HbNEh_WcQYexWxZ8",
-  authDomain: "sirdal-2026.firebaseapp.com",
-  databaseURL: "https://sirdal-2026-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "sirdal-2026",
-  storageBucket: "sirdal-2026.firebasestorage.app",
-  messagingSenderId: "969584723708",
-  appId: "1:969584723708:web:54166b35ffe3595ed6b979"
+
+  location: {
+    name: 'Sinnes, Sirdal',
+    lat: 58.906,
+    lon: 6.850
+  },
+
+  yrUrl: 'https://www.yr.no/nb/v%C3%A6rvarsel/daglig-tabell/2-3138888/Norge/Rogaland/Sirdal/Sinnes',
+
+  firebase: {
+    apiKey: "AIzaSyCgbP4u08tBeNhjIW7HbNEh_WcQYexWxZ8",
+    authDomain: "sirdal-2026.firebaseapp.com",
+    databaseURL: "https://sirdal-2026-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "sirdal-2026",
+    storageBucket: "sirdal-2026.firebasestorage.app",
+    messagingSenderId: "969584723708",
+    appId: "1:969584723708:web:54166b35ffe3595ed6b979"
+  }
 };

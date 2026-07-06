0dc224a6c32f161bc2509aa7518142f775b0ba3d
larsmoller-lamc <larsmollerchristensen@gmail.com>
2026-07-06 13:24:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -26,12 +26,12 @@ window.APP_CONFIG = {
      Lad felterne stå tomme ("") hvis I ikke bruger Firebase — så virker
      siden helt fint lokalt på hver enhed. */
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyCgbP4u08tBeNhjIW7HbNEh_WcQYexWxZ8",
+    authDomain: "sirdal-2026.firebaseapp.com",
+    databaseURL: "https://sirdal-2026-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "sirdal-2026",
+    storageBucket: "sirdal-2026.firebasestorage.app",
+    messagingSenderId: "969584723708",
+    appId: "1:969584723708:web:54166b35ffe3595ed6b979"
   }
 };
